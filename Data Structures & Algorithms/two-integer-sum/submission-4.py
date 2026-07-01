c22a0adee80325ca5678635d37c9836e290ac687
darcode <133553445+darwiniquina@users.noreply.github.com>
2026-07-01 15:55:03 +0800

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        items = {}
+
+        for index, value in enumerate(nums):
+
+            needed = target - value
+
+            if needed in items:
+                return [items[needed], index]
+            else:
+                items[value] = index
