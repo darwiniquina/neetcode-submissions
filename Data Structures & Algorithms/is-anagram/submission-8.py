76bee932c9a2ecbc61ff9c4d7ebfba08b2ab175f
darcode <133553445+darwiniquina@users.noreply.github.com>
2026-07-01 15:21:24 +0800

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        string_counter = {}
+        string_total = 0
+
+        for i in s:
+            
+            if i not in string_counter:
+                string_counter[i] = 0
+
+            string_counter[i] += 1
+            string_total += 1
+
+        for i in t:
+            #E
+            if i not in string_counter:
+                return False
+            else: 
+                string_counter[i] -= 1
+                string_total -= 1
+
+                #E
+                if string_counter[i] < 0:
+                    return False
+
+        return string_total == 0
+
