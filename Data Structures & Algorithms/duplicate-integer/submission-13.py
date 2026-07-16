384ba04b581694c28bba798d43ea0d70bf94b6c4
darcode <133553445+darwiniquina@users.noreply.github.com>
2026-07-16 14:09:50 +0800

    Add: duplicate-integer - submission-13

---
+++ Data Structures & Algorithms/duplicate-integer/submission-13.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        exists = {}
+
+        for num in nums:
+            if num in exists:
+                return True
+
+            exists[num] = True
+
+        return False
